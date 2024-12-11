f8aa6cbce0074a0c07b425d54944b058aafbac76
Evan McCall <Xellos1010@Gmail.com>
2024-12-10 21:50:55 -0500

    (house-keeping) - removed un-used files so that build task and execute successfully. commit the user.interface.ts for prisma lib

--- apps/react-mahi-book-store-backend/src/book/dtos/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './query.dtos';
-export * from './mutation.dtos';
---
+++ libs/prisma/src/interfaces/user/user.interface.ts
@@ -0,0 +1,5 @@
+import { UpdateUserBase, UserBase } from "../../shared/types/user/base.types";
+
+export interface UserInterface extends UserBase {}
+
+export interface UpdateBookInterface extends UpdateUserBase {}
