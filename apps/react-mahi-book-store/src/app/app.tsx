36762b56ff6b1a304ae5f7c052bc7ffa5548a92d
Evan McCall <Xellos1010@Gmail.com>
2024-12-11 01:45:28 -0500

    feat(Books Hook) Added Books hook and tested timeout

--- apps/react-mahi-book-store/src/app/app.tsx
+++ apps/react-mahi-book-store/src/app/app.tsx
@@ -1,14 +1,16 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
 import FilterableBooksCatalog from './FilterableBooksCatalog';
-import mockBooks from '../data/mockBooks'; //Temporary for Static Mock Prototype
-// import NxWelcome from './nx-welcome';
+import useMockBooks from '../hooks/useMockBooks';
 
 export function App() {
+  const { books, isLoading } = useMockBooks();
+
+  if (isLoading) return <div>Loading...</div>;
   return (
     <div>
       {/* <NxWelcome title="react-mahi-book-store" /> */}
-      <FilterableBooksCatalog books={mockBooks}/>
+      <FilterableBooksCatalog books={books}/>
     </div>
   );
 }
